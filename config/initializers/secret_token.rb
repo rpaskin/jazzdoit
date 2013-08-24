1ef260e50b6477ac0a644c1885f19309fbb323c0
Ronnie Paskin <ronnie@megaputz.com>
2013-08-24 13:24:32 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jazzdoit::Application.config.secret_key_base = 'd4bc45f11636a62c697aaedd880832d5881eba2d01a837e8e1c853b2ae4d3ff9034cf99280169d5c0ff544e587d73369d73954fb189c952eaaa71e79d9dc34ba'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jazzdoit::Application.config.session_store :cookie_store, key: '_jazzdoit_session'
