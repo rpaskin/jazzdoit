cbaa6e1da1addd15634fc2338300681930f001b6
Ronnie Paskin <ronnie@megaputz.com>
2013-08-29 23:42:18 -0300

    Conserta testes para password_confirmation; adiciona testes de signup

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -81,13 +81,13 @@ describe UsersController do
         visit edit_user_path(created_user)
         fill_in "user_email", :with => "foobar"+created_user.email
         fill_in "user_password", :with => "foobar"+created_user.password
+        fill_in "user_password_confirmation", :with => "foobar"+created_user.password
         click_button "Update User"
       end
       subject { page }
       it { current_path.should == user_path(User.last) }
       it { should have_content "User was successfully updated" }
       it { should have_content("foobar"+created_user.email) }
-      it { should have_content("foobar"+created_user.password) }
     end
 
     describe "with invalid params" do
@@ -100,7 +100,7 @@ describe UsersController do
       it { current_path.should == user_path(User.last) }
       it { should have_content "Email is invalid" }
       it { should have_field "user_email", with: "foo bar" }
-      it { should have_field "user_password", with: created_user.password }
+      it { should_not have_field "user_password", with: created_user.password }
     end
   end
 
@@ -113,7 +113,6 @@ describe UsersController do
     subject { page }
     it { current_path.should == users_path }
     it { should_not have_content created_user.email }
-    it { should_not have_content created_user.password }
   end
 
   describe "Signup" do
@@ -121,11 +120,44 @@ describe UsersController do
       visit signup_users_path
       fill_in "user_email", :with => built_user.email
       fill_in "user_password", :with => built_user.password
-      click_button "Create User"
+      fill_in "user_password_confirmation", :with => built_user.password
+      expect { click_button "Create User" }.to change(User, :count).by(1)
       visit users_path # TODO visit post signup path for (:id => @user.id)
     end
     subject { page }
     it { should have_content(built_user.email) }
-    it { should have_content(built_user.password) }
+  end
+
+  describe "Signup" do
+    before do
+      visit signup_users_path
+      fill_in "user_email", :with => built_user.email
+      fill_in "user_password", :with => built_user.password
+    end
+
+    describe "valid" do
+      before do
+        fill_in "user_password_confirmation", :with => built_user.password
+        expect { click_button "Create User" }.to change(User, :count).by(1)
+        visit users_path # TODO visit post signup path for (:id => @user.id)
+      end
+      subject { page }
+      it { should have_content(built_user.email) }
+    end
+
+    describe "blank" do
+      before do
+        expect { click_button "Create User" }.not_to change(User, :count)
+      end
+      it { current_path.should == signup_users_path }
+    end
+
+    describe "invalid" do
+      before do
+        fill_in "user_password_confirmation", :with => built_user.password+built_user.password
+        expect { click_button "Create User" }.not_to change(User, :count)
+      end
+      it { current_path.should == signup_users_path }
+    end
   end
 end
