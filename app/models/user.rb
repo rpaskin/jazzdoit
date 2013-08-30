a803ca4a08de453a21c471c799a1204507a0d76d
Ronnie Paskin <ronnie@megaputz.com>
2013-08-29 21:58:31 -0300

    Faz email ser sempre lowercase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   validates :password, presence: true
   validates :email, presence: true, email: true, uniqueness: true
+
+  before_save { self.email = email.downcase }
 end
