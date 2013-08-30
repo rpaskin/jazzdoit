4cd41d5aa51af6e123416dd43a600addaab81c44
Ronnie Paskin <ronnie@megaputz.com>
2013-08-29 22:13:38 -0300

    Muda password para password_hash (irá usar o bcrypt)

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130825041801) do
+ActiveRecord::Schema.define(version: 20130830005239) do
 
   create_table "users", force: true do |t|
     t.string   "email"
-    t.string   "password"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_hash"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,18 +2,18 @@ require 'spec_helper'
 
 describe User do
   before(:each) do
-    @user = User.new(password: "foobar", email: "foo@bar.com")
+    @user = User.new(email: "foo@bar.com", password_hash: "foobar")
   end
 
   subject { @user }
 
-  it { should respond_to(:password) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_hash) }
 
   it { should be_valid }
 
-  describe "when password is not present" do
-    before { @user.password = "" }
+  describe "when password_hash is not present" do
+    before { @user.password_hash = "" }
     it { should_not be_valid }
   end
 
@@ -27,11 +27,19 @@ describe User do
     it { should_not be_valid }
   end
 
-  describe "when email is invalid" do
+  describe "when email is duplicated" do
     before do
       @user.save!
-      @user = User.new(password: "foobar", email: "foo@bar.com")
+      @user = User.new(password_hash: "foobar", email: "foo@bar.com")
     end
     it { should_not be_valid }
+  end
+
+  describe "when email is uppercase" do
+    before do
+      @user = User.new(password_hash: "foobar", email: "AAA@Bbb.com")
+      @user.save!
+    end
+    it { @user.email.should == "aaa@bbb.com" }
   end
 end
