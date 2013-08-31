fe3ade519d35ac79187e9d8c805bfe026b87e7bc
Ronnie Paskin <ronnie@megaputz.com>
2013-08-31 19:31:37 -0300

    Work in progress

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,27 @@ class SessionsController < ApplicationController
   end
 
   def create
-  end
+  	if ok_to_create_user
+  		create_user
+  		# redirect to list
+  	else
+      render 'new'
+	  end
+	end
 
   def destroy
+  end
+
+  private
+
+  def ok_to_create_user
+		if User.find_by(email: params[:session][:email].downcase)
+			flash[:error] = "Email already taken"
+			false
+		end
+		true
+  end
+
+  def create_user
   end
 end
--- spec/features/authentication_pages_spec.rb
+++ spec/features/authentication_pages_spec.rb
@@ -2,14 +2,37 @@ require 'spec_helper'
 
 describe "AuthenticationPages" do
 
+  let(:created_user)     { FactoryGirl.create(:user) }
+  let(:built_user)       { FactoryGirl.build(:user) }
+  let(:valid_attributes) { FactoryGirl.attributes_for(:user) }
+
   subject { page }
 
-  describe "sign-in page" do
+  describe "login page" do
     before do
-      # debugger
-      visit signin_path
+      visit login_path
+    end
+    it { should have_content('Log In') }
+
+    describe "with invalid information" do
+      before { click_button "Log in" }
+      it { should have_title('Log in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
     end
 
-    it { should have_content('Log in') }
+		describe "with valid information" do
+      before do
+        fill_in "Email",    with: created_user.email.upcase
+        fill_in "Password", with: created_user.password
+        click_button "Log in"
+      end
+
+      it { should have_title(user.name) }
+      it { should have_link('Profile',    href: user_path(created_user)) }
+      it { should have_link('Log out',    href: logout_path) }
+      it { should_not have_link('Log in', href: login_path) }
+    end
+
   end
+
 end
