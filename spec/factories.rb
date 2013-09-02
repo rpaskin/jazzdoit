2e5d859096f6a343a0c091a728ee94e02088fc76
Ronnie Paskin <ronnie@megaputz.com>
2013-09-02 09:26:50 -0300

    Adiciona admin user

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,15 @@
 FactoryGirl.define do
+  factory :admin_user do
+    email                 "admin@admin.com"
+    password              "foobar"
+    password_confirmation "foobar"
+  end
+
   factory :user do
     email                 "foo@bar.com"
     password              "foobar"
     password_confirmation "foobar"
   end
-
 
 	factory :list_item do
     description "foobar foobar"
