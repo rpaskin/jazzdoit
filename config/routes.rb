7278d86dde212fed83d283311fce34731e5f51ef
Ronnie Paskin <ronnie@megaputz.com>
2013-09-29 23:09:59 -0300

    Acerta rotas

--- config/routes.rb
+++ config/routes.rb
@@ -12,11 +12,13 @@ Jazzdoit::Application.routes.draw do
   match '/login',  to: 'sessions#new',         via: 'get'
   match '/logout', to: 'sessions#destroy',     via: 'delete'
 
-  get 'user/:id' => 'users#todo_list', as: "user_todo_list"
+  get    'user/:id' => 'users#todo_list',                             as: "user_todo_list"
   delete 'user/:id/destroy_done_items' => 'users#destroy_done_items', as: "user_destroy_done_items"
 
   resources :list_items, only: [:create, :destroy]
-  match '/list_items/:id', to: 'list_items#done', via: ['post', 'patch']
+  match '/list_items/:id',              to: 'list_items#done',        via: ['post', 'patch']
+  match '/list_items/:id/move_lower',   to: 'list_items#move_lower',  via: ['post', 'patch'], as: "list_item_move_lower"
+  match '/list_items/:id/move_higher',  to: 'list_items#move_higher', via: ['post', 'patch'], as: "list_item_move_higher"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
