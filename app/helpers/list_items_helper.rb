9a82b21a34d10d60ef87e94edb1511a313cd1b5a
Ronnie Paskin <ronnie@megaputz.com>
2013-10-14 00:21:08 -0300

    Revert "Salve helper ainda nao utilizado"
    This reverts commit 00d450ee3ce8b8298823e8c9d1adf03064515b2d.

--- app/helpers/list_items_helper.rb
+++
@@ -1,9 +0,0 @@
-module ListItemsHelper
-  def description_tag(list_item)
-    if list_item.url.present?
-      "<a href=\"#{list_item.url}\">#{list_item.description}</a>"
-    else
-      list_item.description
-    end
-  end
-end
