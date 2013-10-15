d74003627f20ea9c6ec1f168e7f87272d5ccc22e
Ronnie Paskin <ronnie@megaputz.com>
2013-10-15 13:14:14 -0300

    Acerta cap de deploy

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,10 +1,17 @@
-root = "/home/ubuntu/rails_apps/jazzdoit"
+root = "/home/ubuntu/rails_apps/cap/Jazzdoit/current"
+
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
- 
+
 #listen "/tmp/unicorn.jazzdoit.sock"
 worker_processes 2
 timeout 30
 listen "127.0.0.1:8080"
+
+# Force the bundler gemfile environment variable to
+# reference the capistrano "current" symlink
+before_exec do |_|
+  ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
+end
