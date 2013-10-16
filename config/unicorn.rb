dd04ca60a56cbf16533a8acc5005cd0f9d974d04
Ronnie Paskin <ronnie@megaputz.com>
2013-10-15 23:19:45 -0300

    Move unicorn para porta 3000

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,7 +8,7 @@ stdout_path "#{root}/log/unicorn.log"
 #listen "/tmp/unicorn.jazzdoit.sock"
 worker_processes 2
 timeout 30
-listen "127.0.0.1:8080"
+listen "127.0.0.1:3000"
 
 # Force the bundler gemfile environment variable to
 # reference the capistrano "current" symlink
